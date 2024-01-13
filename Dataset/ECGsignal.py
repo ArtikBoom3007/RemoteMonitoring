aba81fe7e9e23930a0c5e73aa9b65aeb819d4f72
G Y <54952819+GYBMT1@users.noreply.github.com>
2024-01-13 16:21:39 +0300

    Make data extraction script. Make 12-channel image.

--- Dataset/ECGsignal.py
+++ Dataset/ECGsignal.py
@@ -1,33 +1,35 @@
 import pandas as pd
 import numpy as np
+import sys
 import os
 import matplotlib.pyplot as plt
+from PIL import Image
+
 
 current_directory = os.getcwd()
 
-file_path = os.path.join(current_directory, 'ecg_samples.csv')
+current_directory = os.path.join(current_directory, '../Dataset')
 
 
-def load_func(filename_1, filename_2):
+X,Y = 0, 0
+
+def init():
+    global X,Y
     """
     Загружает данные с двух файлов.
 
     Parameters:
-        filename_1 (str): Путь к npy файлу с записями ЭКГ.
-        filename_2 (str): Путь к csv файлу с данными для датасета.
+        path1 (str): Путь к npy файлу с записями ЭКГ.
+        path2 (str): Путь к csv файлу с данными для датасета.
 
     Returns:
         tuple: Тупль, содержащий X и Y.
     """
-    X = np.load(os.path.join(current_directory, filename_1))
-    Y = pd.read_csv(os.path.join(current_directory, filename_2), sep=';')
-    return X, Y
+    X = np.load(os.path.join(current_directory, 'ecgeq-500hzsrfava.npy'))
+    Y = pd.read_csv(os.path.join(current_directory, 'coorteeqsrafva.csv'), sep=';')
 
 
-X, Y = load_func('ecgeq-500hzsrfava.npy', 'coorteeqsrafva.csv')
-
-
-def plot_signal(selected_value):
+def plot_signal (selected_value):
     """
     Вывести графики для выбранного значения в столбце "Unnamed: 0"
 
@@ -49,7 +51,7 @@ def plot_signal(selected_value):
     # Вывод данных и построение графика
     fig, ax = plt.subplots(12, 1, figsize=(20, 25), sharex=True, sharey=False, constrained_layout=True)
     for i in range(12):
-        print(X[NumberedCase, :2500, i])
+        # print(X[NumberedCase, :2500, i])
         ax[i].plot(X[NumberedCase, :2500, i])
     fig.suptitle(Y.loc[NumberedCase, 'ritmi'], fontsize=25)
 
@@ -58,7 +60,67 @@ def plot_signal(selected_value):
     pass
 
 
-def ECG_signal(selected_value):
+
+def return_signal(selected_value, plot = False):
+    """
+    Вывести графики для выбранного значения в столбце "Unnamed: 0" на одном графике с смещением.
+
+    Parameters:
+    selected_value (int): Значение в столбце "Unnamed: 0"
+
+    Returns:
+    image_array (numpy.ndarray): NumPy array representing the image as uint8
+    """
+
+
+    # Замените 'YourColumnName' на фактическое имя столбца
+    column_name = 'Unnamed: 0'
+
+    # Замените 'YourValue' на значение, которое вы хотите использовать
+    # selected_value = 0  # Замените на фактическое значение
+
+    # Находим индекс, соответствующий выбранному значению в столбце "Unnamed: 0"
+    NumberedCase = Y[Y[column_name] == selected_value].index.item()
+
+    # Вывод данных и построение графика
+    fig, ax = plt.subplots(figsize=(40, 50), constrained_layout=True)
+    plt.axis('off')
+    
+    # Находим максимальное значение амплитуды из всех 12 отведений
+    max_amplitude = np.max(np.abs(X[NumberedCase, :2500, :]))
+
+    for i in range(12):
+        y_offset = i * max_amplitude * 1.5  # Смещение вдоль оси y (больше максимальной амплитуды)
+        ax.plot(X[NumberedCase, :2500, i] + y_offset, label=f'Lead {i + 1}')
+
+    # Получаем массив изображения из текущей фигуры
+    canvas = plt.get_current_fig_manager().canvas
+    canvas.draw()
+    image_data = np.frombuffer(canvas.tostring_rgb(), dtype=np.uint8)
+    image_width, image_height = canvas.get_width_height()
+
+    # Создаем объект изображения
+    image = Image.frombytes("RGB", (image_width, image_height), image_data)
+
+    # Преобразуем изображение в оттенки серого (grayscale)
+    image_gray = image.convert('L')
+
+    # Преобразуем изображение в массив NumPy uint8
+    image_array = np.array(image_gray, dtype=np.uint8)
+
+    # Закрываем текущую фигуру, чтобы не отображать ее
+    plt.close()
+
+    if plot == True:
+        # Отображаем изображение
+        plt.imshow(image_array, cmap='gray', vmin=0, vmax=255)
+        plt.axis('off')
+        plt.show()
+
+    return image_array
+
+
+def signal(selected_value = 1, number_of_signals = 1, rhytm = False, type_of_rhytm = 'SR'):
     """
     Выводит ECG сигнал для выбранного значения в столбце "Unnamed: 0".
 
@@ -71,16 +133,40 @@ def ECG_signal(selected_value):
     # Замените 'YourColumnName' на фактическое имя столбца
     column_name = 'Unnamed: 0'
 
-    # Находим индекс, соответствующий выбранному значению в столбце "Unnamed: 0"
-    NumberedCase = Y[Y[column_name] == selected_value].index.item()
+    if rhytm == True:
+        column_name = 'ritmi'
+        selected_value = type_of_rhytm
+        if number_of_signals == 1:
+            normalCase = random.choice(list(Y[Y['ritmi']==type_of_rhytm].index))
+            ECG_signal_array = np.empty((12, 2500))
+            for i in range(12):
+                ECG_signal_array[i] = X[normalCase, :2500, i]  
+            return ECG_signal_array
+        else:
+            ECG_signal_array = np.empty((number_of_signals, 12, 2500))
+            for j in range(number_of_signals):
+                normalCase = random.choice(list(Y[Y['ritmi']==type_of_rhytm].index))
+                for i in range(12):
+                    ECG_signal_array[j][i] = X[normalCase, :2500, i]
+            return ECG_signal_array
+   
+    if number_of_signals == 1:
+         # Находим индекс, соответствующий выбранному значению в столбце "Unnamed: 0"
+        NumberedCase = Y[Y[column_name] == selected_value].index.item()
+        # Инициализация массива перед циклом
+        ECG_signal_array = np.empty((12, 2500))
 
-    # Инициализация массива перед циклом
-    ECG_signal_array = []
-
-    # Вывод данных
-    for i in range(12):
-        # Добавление строки в виде подмассива в массив
-        ECG_signal_array.append(list(X[NumberedCase, :2500, i]))
+        for i in range(12):
+            # Добавление строки в виде подмассива в массив
+            ECG_signal_array[i] = X[NumberedCase, :2500, i]    
+    else:   
+        ECG_signal_array = np.empty((number_of_signals, 12, 2500))
+        # Вывод данных
+        for j in range(number_of_signals):
+            NumberedCase = Y[Y[column_name] == selected_value[j]].index.item()
+            for i in range(12):
+                # Добавление строки в виде подмассива в подмассив
+                ECG_signal_array[j, i] = X[NumberedCase, :2500, i]
 
     # Возврат всего массива после цикла
     return ECG_signal_array
